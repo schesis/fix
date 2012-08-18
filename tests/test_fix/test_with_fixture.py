54947fd291acb5871176740698e9b6b4f3206b45
Zero Piraeus <z@etiol.net>
2012-08-18 10:17:06 -0400

    Minor test cleanup.

--- tests/test_fix/test_with_fixture.py
+++ tests/test_fix/test_with_fixture.py
@@ -10,17 +10,17 @@ def test_exists():
     assert isinstance(with_fixture, FunctionType)
 
 
-def test_call():
-    """`fix.with_fixture` function does something sensible"""
+def test_setup_only():
+    """`@with_fixture()` handles fixtures which return one function"""
 
-    def my_fixture(context):
+    def fixture(context):
         """Create a fixture that assigns "bar" to `context["foo"]`."""
         def setup():
             """Assign "bar" to `context["foo"]`."""
             context["foo"] = "bar"
         return setup
 
-    @with_fixture(my_fixture)
+    @with_fixture(fixture)
     def case(context):
         """Return `context["foo"]`."""
         return context["foo"]
